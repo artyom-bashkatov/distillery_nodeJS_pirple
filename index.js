52f2eb62f27964cd53de4a67759bdc2e0c08e7bd
Artyom Bashkatov <artyom.bashkatov@distillery.com>
2022-01-31 23:13:43 +0300

    Done: parsing http methods

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ let server = http.createServer((req, res) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g,'')
 
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
   // Send the response
   res.end('Hello World\n');
 
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath)
+  console.log('Request received on path: ' + trimmedPath + ' with method: ' + method)
   
 })
 
