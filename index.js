47d386ad27ed69bfc0c213eaca6b38959a94208a
Artyom Bashkatov <artyom.bashkatov@distillery.com>
2022-02-02 22:46:12 +0300

    Parsing payloads

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 /*
-* Primary file for the API
-*
-*/
+ * Primary file for the API
+ *
+ */
 
 // Dependencies
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // The server should respond to all requests with a string
 let server = http.createServer((req, res) => {
-  
   // Get the URL and parse it
   const parsedUrl = url.parse(req.url, true);
 
   // Get the path
   const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
   // Get the query string as an object
   const queryStringObject = parsedUrl.query;
@@ -25,18 +25,35 @@ let server = http.createServer((req, res) => {
 
   // Get the headers as an object
   const headers = req.headers;
-  console.log('Request received with these headers', headers);
+  console.log("Request received with these headers", headers);
 
-  // Send the response
-  res.end('Hello World\n');
+  // Get the payload, if any
+  const decoder = new StringDecoder("utf8");
+  let buffer = "";
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
 
-  // Log the request path
-  console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' with this query string parameters' + JSON.stringify(queryStringObject))
-  
-})
+    // Send the response
+    res.end("Hello World\n");
+    console.log("Request received with this payload", buffer);
+
+    // Log the request path
+    console.log(
+      "Request received on path: " +
+        trimmedPath +
+        " with method: " +
+        method +
+        " with this query string parameters" +
+        JSON.stringify(queryStringObject)
+    );
+  });
+});
 
 server.listen(3000, () => {
-  console.log("The server is listening on port 3000 now")
-})
+  console.log("The server is listening on port 3000 now");
+});
 
 // Start the server, and have it listen on port 3000
