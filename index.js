4dacd30c4a0fa8f4fe6bbf41613ef42187597935
Artyom Bashkatov <artyom.bashkatov@distillery.com>
2022-02-07 15:45:13 +0300

    Adding HTTPS Support

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 
 // Dependencies
 const http = require("http");
+const https = require('https');
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 const config = require('./config');
+const fs = require('fs');
 
 // Define the handlers
 const handlers = {};
@@ -30,26 +32,25 @@ const router = {
 }
 
 
-
-// The server should respond to all requests with a string
-let server = http.createServer((req, res) => {
+// All the server logic for both- the http and https server
+const unifiedServer = (req, res) => {
   // Get the URL and parse it
   const parsedUrl = url.parse(req.url, true);
-
+  
   // Get the path
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
-
+  
   // Get the query string as an object
   const queryStringObject = parsedUrl.query;
-
+  
   // Get the HTTP Method
   const method = req.method.toLowerCase();
-
+  
   // Get the headers as an object
   const headers = req.headers;
   console.log("Request received with these headers", headers);
-
+  
   // Get the payload, if any
   const decoder = new StringDecoder("utf8");
   let buffer = "";
@@ -58,10 +59,10 @@ let server = http.createServer((req, res) => {
   });
   req.on("end", () => {
     buffer += decoder.end();
-
+  
     // Choose the handler this request should go to. If one is not found, use the notFound handler
     const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
+  
     // Construct the data object to send to the handler
     const data = {
       'trimmedPath': trimmedPath,
@@ -70,28 +71,28 @@ let server = http.createServer((req, res) => {
       'headers': headers,
       'payload': buffer
     }
-
+  
     // Route the request to the handler specified in the router
     chosenHandler(data, (statusCode, payload) => {
       statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
       payload = typeof(payload) == 'object' ? payload : {};
-
+  
       // Convert the payload to a string
       const payloadString = JSON.stringify(payload);
-
+  
       // Return the response
       res.setHeader('Content-Type', 'application/json');
       res.writeHead(statusCode);
       res.end(payloadString);
-
+  
       // Log the request path
       console.log("Returning this response", statusCode, payloadString);
     })
-
+  
     // Send the response
     // res.end("Hello World\n");
     console.log("Request received with this payload", buffer);
-
+  
     // Log the request path
     console.log(
       "Request received on path: " +
@@ -102,9 +103,30 @@ let server = http.createServer((req, res) => {
         JSON.stringify(queryStringObject)
     );
   });
+  }
+
+
+
+// The server should respond to all requests with a string
+let httpServer = http.createServer((req, res) => {
+  unifiedServer(req, res);
 });
 
 // Start the server
-server.listen(config.port, () => {
-  console.log("The server is listening on port " + config.port + " " + config.envName + " now");
-}); 
+httpServer.listen(config.httpPort, () => {
+  console.log("The server is listening on port " + config.httpPort + " " + config.envName + " now");
+});
+
+// Instantiate the HTTPS server
+const httpsServerOptions = {
+  'key' : fs.readFileSync('./https/key.pem-'),
+  'cert' : fs.readFileSync('./https/cert.pem')
+};
+let httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+  unifiedServer(req, res);
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, () => {
+  console.log("The server is listening on port " + config.httpsPort + " " + config.envName + " now");
+});
