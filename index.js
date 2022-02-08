a69e7b271f41727580dab6ebd729c6e6dc18edc7
Artyom Bashkatov <artyom.bashkatov@distillery.com>
2022-02-08 15:53:34 +0300

    Service ping

--- index.js
+++ index.js
@@ -5,52 +5,49 @@
 
 // Dependencies
 const http = require("http");
-const https = require('https');
+const http2 = require("http2");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
-const config = require('./config');
-const fs = require('fs');
+const config = require("./config");
+const fs = require("fs");
 
 // Define the handlers
 const handlers = {};
 
-
-// Sample handler
-handlers.sample = (data, callback) => {
-  // Calback a http status code, and a payload object
-  callback(406, {'name': 'sample handler'})
+// Ping handler
+handlers.ping = (data, callback) => {
+  callback(200);
 };
 
-// Not found handler
+// NotFound handler
 handlers.notFound = (data, callback) => {
   callback(404);
-}
+};
 
 // Define a request router
 const router = {
-  'sample' : handlers.sample
-}
-
+  ping: handlers.ping,
+};
 
 // All the server logic for both- the http and https server
 const unifiedServer = (req, res) => {
   // Get the URL and parse it
   const parsedUrl = url.parse(req.url, true);
-  
+
   // Get the path
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
-  
+
   // Get the query string as an object
   const queryStringObject = parsedUrl.query;
-  
+
   // Get the HTTP Method
   const method = req.method.toLowerCase();
-  
+
   // Get the headers as an object
   const headers = req.headers;
   console.log("Request received with these headers", headers);
-  
+
   // Get the payload, if any
   const decoder = new StringDecoder("utf8");
   let buffer = "";
@@ -59,40 +56,43 @@ const unifiedServer = (req, res) => {
   });
   req.on("end", () => {
     buffer += decoder.end();
-  
+
     // Choose the handler this request should go to. If one is not found, use the notFound handler
-    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-  
+    const chosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
     // Construct the data object to send to the handler
     const data = {
-      'trimmedPath': trimmedPath,
-      'queryStringObject': queryStringObject,
-      'method': method,
-      'headers': headers,
-      'payload': buffer
-    }
-  
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer,
+    };
+
     // Route the request to the handler specified in the router
     chosenHandler(data, (statusCode, payload) => {
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-      payload = typeof(payload) == 'object' ? payload : {};
-  
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
+      payload = typeof payload == "object" ? payload : {};
+
       // Convert the payload to a string
       const payloadString = JSON.stringify(payload);
-  
+
       // Return the response
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Content-Type", "application/json");
       res.writeHead(statusCode);
       res.end(payloadString);
-  
+
       // Log the request path
       console.log("Returning this response", statusCode, payloadString);
-    })
-  
+    });
+
     // Send the response
     // res.end("Hello World\n");
     console.log("Request received with this payload", buffer);
-  
+
     // Log the request path
     console.log(
       "Request received on path: " +
@@ -103,9 +103,13 @@ const unifiedServer = (req, res) => {
         JSON.stringify(queryStringObject)
     );
   });
-  }
+};
 
-
+// Instantiate the HTTPS server
+const httpsServerOptions = {
+  key: fs.readFileSync("./https/key.pem-"),
+  cert: fs.readFileSync("./https/cert.pem"),
+};
 
 // The server should respond to all requests with a string
 let httpServer = http.createServer((req, res) => {
@@ -114,19 +118,26 @@ let httpServer = http.createServer((req, res) => {
 
 // Start the server
 httpServer.listen(config.httpPort, () => {
-  console.log("The server is listening on port " + config.httpPort + " " + config.envName + " now");
+  console.log(
+    "The server is listening on port " +
+      config.httpPort +
+      " " +
+      config.envName +
+      " now"
+  );
 });
 
-// Instantiate the HTTPS server
-const httpsServerOptions = {
-  'key' : fs.readFileSync('./https/key.pem-'),
-  'cert' : fs.readFileSync('./https/cert.pem')
-};
-let httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+let httpsServer = http2.createSecureServer(httpsServerOptions, (req, res) => {
   unifiedServer(req, res);
 });
 
 // Start the HTTPS server
 httpsServer.listen(config.httpsPort, () => {
-  console.log("The server is listening on port " + config.httpsPort + " " + config.envName + " now");
+  console.log(
+    "The server is listening on port " +
+      config.httpsPort +
+      " " +
+      config.envName +
+      " now"
+  );
 });
