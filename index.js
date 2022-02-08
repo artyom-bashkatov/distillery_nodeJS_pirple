d7680aa2b1a42933b63a39e4be3dd4af7b6d2e13
Artyom Bashkatov <artyom.bashkatov@distillery.com>
2022-02-08 17:10:55 +0300

    Storing Data

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 const config = require("./config");
 const fs = require("fs");
+const _data = require('./lib/data');
+
+// TESTING
+// @TODO delete this
+/* _data.create('test', 'newFile', {'foo':'bar'}, (err) => {
+  console.log("this was the error", err)
+})
+
+_data.read('test', 'newFile',(err, data) => {
+  console.log("this was the error", err, 'and this was the data', data)
+}) 
+
+_data.update('test', 'newFile', {'fizz':'buzz'}, (err) => {
+  console.log("this was the error", err)
+})
+
+_data.delete('test', 'newFile', (err) => {
+  console.log("this was the error", err)
+}) */
 
 // Define the handlers
 const handlers = {};
